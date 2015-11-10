42aa63a665b66965b752c01898b94d5fe526c1e5
fuennelgithub <fuennel@gmail.com>
2015-11-10 14:59:43 -0500

    checking in layout and navbar and twitter and facebook icons
    navbar.php includes the twitter and facebook hyperlinks icons.
    layout.php includes the formatting for the icons.

--- navbar.php
+++ navbar.php
@@ -1,24 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link type="text/css" rel="stylesheet" href="layout.css">
+</head>	
+<body>
+
+	<div id="social_media">
+	<a id="faceb" href="http://www.facebook.com" style="height: 100px; width: 100px;"> <img src="images/faceb.jpg" class="icons"> </img> </a>
+	<a id="twitter" href="https://twitter.com/"> <img src="images/twitter.jpg" class="icons"> </img> </a>
+	</div>
+	
 	<div id="banner">
-		<img id="pets_image" src="images/AnimalCareBanner.jpg" width="100%">
+		KozyCorner
+	</div>
+	
+	<div>
+    <img id="pets_image" src="images/AnimalCareBanner.jpg" width="1138" height="150">
 	</div>
 	
 	<div id="navbar">
-	<div class="wrapper">
 	<ul>
-		<li><a href="index.php">Home</a></li>
-		<li><a href="lost.php">Lost Pets</a></li>
-		<li><a href="found.php">Found Pets</a></li>
-		<li><a href="rescued.php">Adopt a Pet</a></li>
-		<li><a href="postform.php">Report</a></li>
-		<li><a href="faq.php">FAQ</a></li>
+		<li><a href="index.html">Home</a></li>
+		<li><a href="rescued.php">Rescued</a></li>
+		<li><a href="found.php">Found</a></li>
+		<li><a href="lost.php">Lost</a></li>
+		<li><a href="post.html">Report</a></li>
+		<li><a href="faq.html">FAQ</a></li>		
 		<?php
 			if (isSet($_COOKIE["user_id"])) {
-				print "<li class=\"account\"><a href=\"logout.php\">Logout</a></li>";
+				print "<li><a href=\"logout.php\">Logout</a></li>";
 			} else {
-				print "<li class=\"account\"><a href=\"login.php\">Login</a></li>";
-				print "<li class=\"account\"><a href=\"signup.php\">Register</a></li>";
+				print "<li><a href=\"login.php\">Login</a></li>";
+				print "<li><a href=\"signup.html\">Register</a></li>";
 			}
-		?>
+		?>		
 	</ul>
 	</div>
-	</div>
+
+</body>
+</html>
