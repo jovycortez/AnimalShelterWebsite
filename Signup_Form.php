dcfc13d91e0e2a157c0aafb5707cf72214696c21
Jovy Cortez <jovycortez11@yahoo.com>
2015-11-10 15:37:57 -0500

    Signup Form and the PHP

---
+++ Signup_Form.php
@@ -0,0 +1,116 @@
+<!-- Signup form -->
+<!--
+To get to signup form:
+Click Post (if not a user.)
+Click Login, then click create account.
+
+Fields:
+Username
+Password
+Email Address
+First Name
+Last Name
+Zip code 
+
+-->
+<!DOCTYPE html>
+<html>
+<html lang = "en">
+	<div>
+		<head>
+			<title>Sign Up</title>
+			<link rel="stylesheet" type="text/css" href="layout.css" />
+		</head>
+	</div>
+ 
+		<body>
+		<?php
+		<include ("navbar.php");
+		?>
+		<div class="wrapper">
+		<form action = "Signup.php" method ="post">
+			<form>
+			 <a href="login.php"> Already have an account? Login here.</a>
+				<h2> Welcome </h2>
+					<div>
+					<table>
+						<table width="500px"> 
+						 <tr>
+						<td>
+							<label for="username">Username</label>
+						</td>
+						<td>
+							
+							<input type="text" name="username" maxlength="100" size="30" required>
+						</td>
+						</tr>
+						<tr>
+						<td>
+							<label for="password">Password</label>
+						</td>
+						<td>
+							<input type="text" name="password" maxlength="30" size="30" required>
+						</td>
+						</tr>
+						<tr>
+						<td>
+							<label for="email">Email Address</label>
+						</td>
+						<td>
+							<input type="text" name="email" maxlength="100" size="30" required>
+						</td>
+						</tr>
+						<tr>
+						<td>
+							<label for="firstName">First Name</label>
+						</td>
+						<td>
+							<input type="text" name="firstName" maxlength="100" size="30"  required>
+						</td>
+						</tr>
+						<tr>
+						<td>
+							<label for="lastName">Last Name</label>
+						</td>
+						<td>
+							<input type="text" name="lastName" maxlength="100" size="30" required>
+						</td>
+						</tr>
+												<tr>
+						<td>
+							<label for="zip">Zip Code</label>
+						</td>
+						<td>
+							<input type="text" name="zip" maxlength="100" size="30" required>
+						</td>
+						</tr>
+						<tr>
+						<td>
+							<label for="phone">Phone Number</label>
+						</td>
+						<td>
+							<input type="text" name="phone" maxlength="10" size="30" required>
+						</td>
+						</tr>
+						
+						<tr>
+						<td>
+							<label for="userType"></label>
+						</td>
+						<td>
+							<input type="text" name="phone" maxlength="10" size="30" required>
+						</td>
+						</tr>
+						
+						</table>
+						<button type="submit" value="Submit">Submit</button>
+						   
+			</fieldset>
+			</form>	
+			
+			</div>
+
+</div>
+
+	</body>	
+</html>
--- signup.php
+++ signup.php
@@ -1,189 +1,128 @@
-<html>
-<div>
-<head>
-	<title>Sign Up</title>
-</head>
-</div>
-<body>
-<div>
-<form>
-Sign up       Already have an account? Sign in here.
-<fieldset>
-<legend>Personal Information</legend>
-<table width="450px">
-</tr>
-<tr>
-<td>
-	<label for="first_name">First Name</label>
-</td>
-<td>
-	<input type="text" name="first_name" maxlength="100" size="30"  required>
-</td>
-</tr>
-<tr>
-<td>
-	<label for="last_name">Last Name</label>
-</td>
-<td>
-	<input type="text" name="last_name" maxlength="100" size="30"
-</td>
-</tr>
-<tr>
-<td>
-	<label for="email">Email Address</label>
-</td>
-<td>
-	<input type="text" name="email" maxlength="100" size="30"
-</td>
-</tr>
+<!--Signup php-->
 
-<tr>
-<td>
-	<label for="address_line_1">Address Line 1</label>
-</td>
-<td>
-	<input type="text" name="address_line_1" maxlength="100" size="30" >
-	<br/> Street address, P.O. box, company name, c/o
-</td>
-</tr>
-
-<tr>
-<td>
-	<label for="address_line_2">Address Line 2</label>
-</td>
-<td>
-	<input type="text" name="address_line_2" maxlength="100" size=size="10" > 
-	<br/> Apartment, suite, unit, building, floor, etc. </label>  
-</td>
-</tr>
-
-<tr>
-<td>
-	<label for="city">City</label>
-</td>
-<td>
-	<input type="text" name="city" maxlength="100" size="30" >
-</td>
-</tr>
-
-<tr>
-<td>
-	<label for="state">State</label>
-</td>
-<td>
-	<input type="text" name="state" maxlength="100" size="30" >
-</td>
-</tr>
-
-<tr>
-<td>
-	<label for="zip_code">Zip Code</label>
-</td>
-<td>
-	<input type="text" name="zip_code" maxlength="100" size="30" >
-</td>
-</tr>
-
-<tr>
-<td>
-	<label for="phone">Phone Number</label>
-</td>
-<td>
-	<input type="text" name="phone" maxlength="10" size="30"
-</td>
-</tr>
-
-</table>
-	</fieldset>
-	<br/> <br/>
-	<fieldset >
-	<legend>Pet Description</legend>
-<table width="400px">
-</tr>
-
-<tr>
-<td>
-	<label for="pet_name">Pet's Name</label>
-</td>
-<td>
-	<input type="text" name="pet_name" maxlength="100" size="30" >
-</td>
-</tr>
-
-<tr>
-<td>
-	<label for="pet_type">What kind of animal? (example: dog) </label>
-</td>
-<td>
-	<input type="text" name="pet_type" maxlength="100" size="30" >
-</td>
-</tr>
-
-<tr>
-<td>
-	<label for "breed"> Breed</label>
-</td>
-<td>
-	<input type="text" name="breed" maxlength="100" size="30" >
-</td>
-</tr>
-
-<tr>
-<td>
-	<label for="fur_color">Fur Color</label>
-</td>
-<td>
-	<input type="text" name="fur_color" maxlength="100" size="30" >
-</td>
-</tr>
-
-<tr>
-<td>
-	<label for="age">Age</label>
-</td>
-<td>
-	<input type="text" name="age" maxlength="100" size="30" >
-</td>
-</tr>
-
-<tr>
-<td>
-	<label for="weight">Weight</label>
-</td>
-<td>
-	<input type="text" name="weight" maxlength="100" size="30" >
-</td>
-</tr>
-
-<tr>
-<td>
-	<label for="date_lost">Date Lost/Found</label>
-</td>
-<td>
-	<input type="text" name="date_lost" maxlength="100" size="30" >
-</td>
-</tr>
-
-<tr>
-<td>
-	<label for="has_tag"></label>
-</td>
-<td>
-	<input type="text" name="pet_name" maxlength="100" size="30" >
-</td>
-</tr>
-
-
-	<tr>
-<td>
-	<label for="pet_description">Pet Description</label>
-</td>
-<td>
-	<textarea name="pet_description" maxlength="1000" cols="30" rows="6"></textarea>
-</td>
-</tr>
-	</fieldset>
-</form>
-</div>
-</body>	
+<html lang = "en">
+	<head>
+		<title> Form </title>
+		<meta charset = "utf-8" />
+		<link rel="stylesheet" type="text/css" href="layout.css" />
+	</head>
+	<body>
+	
+	<?php
+	include ('navbar.php');
+	include('kozycorner.dbconfig.inc');
+	include ('footer.php');
+	
+	// Connect to MySQL	
+	$conn = mysqli_connect($hostname,$username, $password, $dbname);
+	if (mysqli_connect_errno()) {
+		 print "Connection failed: " . mysqli_connect_error();
+		 exit;
+	}
+	
+		//Set variables for user
+		#$section = $_POST['section'];
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+		$email = $_POST['email'];
+		$firstName= $_POST['firstName'];
+		$lastName = $_POST['lastName'];
+		$zip = $_POST['zip'];
+		$phone = $_POST['phone'];
+		
+		
+		//All strings becomes uppercase
+		$username = strtoupper($username);
+		$password = strtoupper($password);
+		$email = strtoupper($email);
+		$firstName= strtoupper($firstName);
+		$lastName = strtoupper($lastName);
+		$zip = strtoupper($zip);
+		$phone = strtoupper($phone);
+		
+		
+		//Selects and runs to hold breed table data
+		$query = "SELECT * FROM User";
+		$result = mysqli_query($conn, $query);
+		if (!$result) {
+			print "Error. Cannot be executed" . mysqli_error($conn);
+			exit;
+		}
+		
+		$num_rows = mysqli_num_rows($result);
+		$row = mysqli_fetch_assoc($result);
+		
+		
+		// Move to the next row
+		$row = mysqli_fetch_assoc($result);
+		}
+		
+		//Checks if email/username exists
+		if($exist == FALSE){
+			
+			//Adds to User table in Database
+			$query = "INSERT INTO mydb.user(username) VALUES('$username')";
+			$result = mysqli_query($conn, $query);
+			if (!$result) {
+				print "Error";
+					mysql_error();
+				exit;
+			}
+			
+			$query = "SELECT * FROM USER";
+			// Run query
+			$result = mysqli_query($conn, $query);
+			if (!$result) {
+				print "Error " . mysqli_error($conn);
+				exit;
+			}
+			//Selects and runs to hold breed table data
+			$num_rows = mysqli_num_rows($result);
+			$row = mysqli_fetch_assoc($result);
+		
+			$exist = FALSE;
+			$row = mysqli_fetch_assoc($result);
+			}
+			
+		}
+		
+		//Inserts a new user
+		$query = "INSERT INTO mydb.user (user_id,username,password, email, first_name,last_name, zip, phone, user_type)
+			VALUES('','$user_name', '$password', '$email', '$firstName', '$lastName', '$zip', '$phone', $userType)";
+		$result = mysqli_query($conn, $query);
+		if (!$result) {
+			print "Error - the insert query could not be executed";
+				mysql_error();
+			exit;	
+		}
+		
+		
+		//Links post_id to user
+		$userid = $_COOKIE["user_id"];
+		$query = "INSERT INTO mydb.users_pets (pet_id, user_id) VALUES ('$petid', '$userid')";
+		$result = mysqli_query($conn, $query);
+		if (!$result) {
+			print "Error - the latest query could not be executed";
+				mysql_error();
+			exit;	
+		}
+		?>
+		<div class="wrapper">
+		<h2> Thanks for signing up!</h2>
+		<a href="home.php"> home </a><br/>
+		<a href="PostForm.php"> Report </a>
+		</div>
+	</body>
+	</head>
 </html>
+
+
+
+
+
+
+
+
+
+
+
